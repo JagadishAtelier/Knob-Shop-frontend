473dec35d449fc3e75f9e00d73cdf6ea78b9d992
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-05-21 19:21:13 +0530

    worked from brand to footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import 'modern-css-reset/dist/reset.min.css';
 import './App.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarTop from './Components/Navbar/NavbarTop/NavbarTop';
+// import NavbarTop from './Components/Navbar/NavbarTop/NavbarTop';
+import Footer from './Components/Footer/Footer';
+import Location from './Components/Location/Location';
+import OurServices from './Components/OurServices/OurServices';
+import FAQ from './Components/FAQ/FAQ';
+import Testimonals from './Components/Testimonals/Testimonals';
+import Brand from './Components/Brand/Brand';
 function App() {
   return (
     <div>
-      <NavbarTop/>
+      {/* <NavbarTop/> */}
+      <Brand/>
+      <Testimonals/>
+      <FAQ/>
+      <OurServices/>
+      <Location/>
+      <Footer/>
     </div>
   )
 }
